b55e36e0e11eec9a723b85a62560f108eb234aff
Ricter Z <ricterzheng@gmail.com>
2022-12-17 21:16:41 +0800

    update README.md

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup, Extension
 encode = Extension('qrencode_ascii._qrencode', sources=['qr_encode.c'], libraries=['qrencode'])
 
 setup(name='qrencode-ascii',
-      version='1.0',
+      version='1.0.1',
       description='qrencode console version',
       author='Ricter Zheng',
       author_email='ricterzheng@gmail.com',
       url='https://github.com/RicterZ/pyqrencode-ascii/tree/master',
-      long_description='''A simple wrapper for the C qrencode library.''',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
       packages=['qrencode_ascii'],
       ext_modules=[encode],
       requires=[])
